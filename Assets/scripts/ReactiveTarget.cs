f73c78ac30bfa8c8fd9a49cdbb6f290f5b008768
ivanaimufua41 <30399277+ivanaimufua41@users.noreply.github.com>
2018-08-16 20:48:16 -0600

    first upload

---
+++ Assets/scripts/ReactiveTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactiveTarget : MonoBehaviour {
+
+	// Use this for initialization
+	public void ReactToHit () {
+        Wandering_Ai behaviour = GetComponent<Wandering_Ai>();
+        if (behaviour != null)
+        {
+            behaviour.SetAlive(false);
+        }
+        StartCoroutine(Die());
+	}
+	
+    private IEnumerator Die()
+    {
+        this.transform.Rotate(-75, 0, 0);
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(this.gameObject);
+
+    }
+
+	// Update is called once per frame
+	
+}
